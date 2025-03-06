6fcdfa0828ab9360eda16500293745cb27e523fb
rashik <mhdrashik112@gmail.com>
2025-03-06 17:51:48 +0530

    Removed unnecessary files from tracking

--- Node.js/BasicProgram/condition.js
+++
@@ -1,5 +0,0 @@
-var read = require('readline-sync')
-
-var value = read.question('What is your name : ')
-
-console.log(value)
--- Node.js/hello.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello world')
-
-var name = ('Rashik')
-var name1 = new String('peek')
-
-console.log(name)
