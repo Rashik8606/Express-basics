8980a3e822c75be1be8bfb44bee210369078e6fd
rashik <mhdrashik112@gmail.com>
2025-03-14 01:35:34 +0530

    Start Movie Platform

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const exphbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+
+app.engine('hbs', exphbs.engine({ 
+  extname: '.hbs', 
+  defaultLayout: 'main', 
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir:__dirname+'/views/partials/'
+}));
 app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
 
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -8,33 +8,41 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  moviesList = [
+    {
+      title : "Thriller",
+      year : 2024,
+      discription : "The adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.",
+      moviepic : 'https://th.bing.com/th/id/OIP.wRrh_biYR_xkAiHCn4HePgHaK9?rs=1&pid=ImgDetMain'
+    
+    },
+    {
+      title : "The Godfather",
+      year : 1972,
+      discription : "The The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son. of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.",
+      moviepic : 'https://th.bing.com/th/id/OIP.eqAqwN06RtHHpw_23vCbkgHaE8?rs=1&pid=ImgDetMain'
+    
+    },
+    {
+      title : " Interstellar",
+      year : 2014,
+      discription : "The When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans. of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.",
+      moviepic : 'https://th.bing.com/th/id/OIP.JymutQOaBSxOAXmuij9g-wAAAA?rs=1&pid=ImgDetMain'
+    
+    },
+    {
+      title : "Avengers EndGame",
+      year : 2024,
+      discription : "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos actions and restore balance to the universe.",
+      moviepic : 'https://th.bing.com/th/id/OIP.8Y0Z68LfmR7EfZNOq30b7wHaHa?rs=1&pid=ImgDetMain'
+    
+    }
+  ]
+  console.log(moviesList)
+  res.render('index', {moviesList,admin:true});
 });
 
-router.post('/submit', function (req, res) {
-  console.log('Received data:', req.body);
-  MongoClient.connect('mongodb://127.0.0.1:27017', function (err, client) {
-      if (err) {
-          console.error('Database connection error:', err);
-          return res.status(500).send('Database connection error');
-      }
-      console.log('Database connected successfully');
 
-      const db = client.db('persondata');
-      const collection = db.collection('users');
-
-      collection.insertOne(req.body, function (err, result) {
-          if (err) {
-              console.error('Data insertion error:', err);
-              return res.status(500).send('Data Not Inserted');
-          } else {
-              console.log('Data Inserted Successfully:', result);
-              res.send('Data inserted successfully!');
-          }
-          client.close();
-      });
-  });
-});
 
 
 module.exports = router;
