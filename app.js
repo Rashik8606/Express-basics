1ae8f2b16970560ca84b901d6688cc640bfdaddd
rashik <mhdrashik112@gmail.com>
2025-04-22 18:10:51 +0530

    Change app

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
   
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
